a209ce6cfb2559f2f12f06592456867717086723
Steve Lhomme <robux4@gmail.com>
2013-01-30 17:06:44 +0100

    add a callback to let the logger know it's being called, that's useful to make sure the logging file is available

--- library/src/com/levelup/logutils/FLogger.java
+++ library/src/com/levelup/logutils/FLogger.java
@@ -1,57 +1,76 @@
 package com.levelup.logutils;
 
+import com.levelup.logutils.FLog.FLogLevel;
+
 
 /**
  * helper class to use the FLog API from a class rather than using the static methods
  */
 public class FLogger {
 
+	/**
+	 * method called for every log call in case the file logger wasn't ready before
+	 * 
+	 * @param level
+	 */
+	protected void assertLogger(FLogLevel level) {}
+	
 	public int v(String tag, String message) {
+		assertLogger(FLogLevel.V);
 		FLog.v(tag, message);
 		return 0;
 	}
 
 	public int v(String tag, String message, Throwable tr) {
+		assertLogger(FLogLevel.V);
 		FLog.v(tag, message, tr);
 		return 0;
 	}
 
 	public int d(String tag, String message) {
+		assertLogger(FLogLevel.D);
 		FLog.d(tag, message);
 		return 0;
 	}
 
 	public int d(String tag, String message, Throwable tr) {
+		assertLogger(FLogLevel.D);
 		FLog.d(tag, message, tr);
 		return 0;
 	}
 
 	public int i(String tag, String message) {
+		assertLogger(FLogLevel.I);
 		FLog.i(tag, message);
 		return 0;
 	}
 
 	public int i(String tag, String message, Throwable tr) {
+		assertLogger(FLogLevel.I);
 		FLog.i(tag, message, tr);
 		return 0;
 	}
 
 	public int w(String tag, String message) {
+		assertLogger(FLogLevel.W);
 		FLog.w(tag, message);
 		return 0;
 	}
 
 	public int w(String tag, String message, Throwable tr) {
+		assertLogger(FLogLevel.W);
 		FLog.w(tag, message, tr);
 		return 0;
 	}
 
 	public int e(String tag, String message) {
+		assertLogger(FLogLevel.E);
 		FLog.e(tag, message);
 		return 0;
 	}
 
 	public int e(String tag, String message, Throwable tr) {
+		assertLogger(FLogLevel.E);
 		FLog.e(tag, message, tr);
 		return 0;
 	}
--- library/src/com/levelup/logutils/FLoggerTagged.java
+++ library/src/com/levelup/logutils/FLoggerTagged.java
@@ -1,5 +1,7 @@
 package com.levelup.logutils;
 
+import com.levelup.logutils.FLog.FLogLevel;
+
 
 /**
  * helper class to use the FLog from a class rather than using the static methods
@@ -8,47 +10,65 @@ package com.levelup.logutils;
 public class FLoggerTagged {
 	protected final String tag;
 	
+	/**
+	 * method called for every log call in case the file logger wasn't ready before
+	 * 
+	 * @param level
+	 */
+	protected void assertLogger(FLogLevel level) {}
+	
 	public FLoggerTagged(String tag) {
+		assertLogger(FLogLevel.V);
 		this.tag = tag;
 	}
 
 	public void v(String message) {
+		assertLogger(FLogLevel.V);
 		FLog.v(tag, message);
 	}
 
 	public void v(String message, Throwable tr) {
+		assertLogger(FLogLevel.V);
 		FLog.v(tag, message, tr);
 	}
 
 	public void d(String message) {
+		assertLogger(FLogLevel.D);
 		FLog.d(tag, message);
 	}
 
 	public void d(String message, Throwable tr) {
+		assertLogger(FLogLevel.D);
 		FLog.d(tag, message, tr);
 	}
 
 	public void i(String message) {
+		assertLogger(FLogLevel.I);
 		FLog.i(tag, message);
 	}
 
 	public void i(String message, Throwable tr) {
+		assertLogger(FLogLevel.I);
 		FLog.i(tag, message, tr);
 	}
 
 	public void w(String message) {
+		assertLogger(FLogLevel.W);
 		FLog.w(tag, message);
 	}
 
 	public void w(String message, Throwable tr) {
+		assertLogger(FLogLevel.W);
 		FLog.w(tag, message, tr);
 	}
 
 	public void e(String message) {
+		assertLogger(FLogLevel.E);
 		FLog.e(tag, message);
 	}
 
 	public void e(String message, Throwable tr) {
+		assertLogger(FLogLevel.E);
 		FLog.e(tag, message, tr);
 	}
 }
