3c691e5d92154f27cd720128c9f5b4909ec9dcf5
Steve Lhomme <robux4@gmail.com>
2014-06-04 08:05:22 +0200

    Add a tagged logger that wraps a FLogger (in case it's not the default one, like in FLoggerTagged)

---
+++ library/src/com/levelup/logutils/FLoggerWrapTagged.java
@@ -0,0 +1,95 @@
+package com.levelup.logutils;
+
+/**
+ * A logger with a hard coded tag that send logs to the supplied FLogger 
+ */
+public class FLoggerWrapTagged {
+	protected final String tag;
+	protected final FLogger logger;
+
+	/**
+	 * method called for every log call in case the file logger wasn't ready before
+	 * 
+	 * @param level
+	 */
+	protected void assertLogger(FLogLevel level) {}
+	
+	public FLoggerWrapTagged(FLogger logger, String tag) {
+		assertLogger(FLogLevel.V);
+		this.tag = tag;
+		this.logger = logger;
+	}
+
+	public int v(String message) {
+		assertLogger(FLogLevel.V);
+		logger.v(tag, message);
+		return 0;
+	}
+
+	public int v(String message, Throwable tr) {
+		assertLogger(FLogLevel.V);
+		logger.v(tag, message, tr);
+		return 0;
+	}
+
+	public int d(String message) {
+		assertLogger(FLogLevel.D);
+		logger.d(tag, message);
+		return 0;
+	}
+
+	public int d(String message, Throwable tr) {
+		assertLogger(FLogLevel.D);
+		logger.d(tag, message, tr);
+		return 0;
+	}
+
+	public int i(String message) {
+		assertLogger(FLogLevel.I);
+		logger.i(tag, message);
+		return 0;
+	}
+
+	public int i(String message, Throwable tr) {
+		assertLogger(FLogLevel.I);
+		logger.i(tag, message, tr);
+		return 0;
+	}
+
+	public int w(String message) {
+		assertLogger(FLogLevel.W);
+		logger.w(tag, message);
+		return 0;
+	}
+
+	public int w(String message, Throwable tr) {
+		assertLogger(FLogLevel.W);
+		logger.w(tag, message, tr);
+		return 0;
+	}
+
+	public int e(String message) {
+		assertLogger(FLogLevel.E);
+		logger.e(tag, message);
+		return 0;
+	}
+
+	public int e(String message, Throwable tr) {
+		assertLogger(FLogLevel.E);
+		logger.e(tag, message, tr);
+		return 0;
+	}
+
+	public int wtf(String message) {
+		assertLogger(FLogLevel.WTF);
+		logger.wtf(tag, message);
+		return 0;
+	}
+
+	public int wtf(String message, Throwable tr) {
+		assertLogger(FLogLevel.WTF);
+		logger.wtf(tag, message, tr);
+		return 0;
+	}
+
+}
